50d825255fcea20594270e0a931d845543ebde53
N3XUS123 <lPalma.esp123@gmail.com>
2018-11-05 09:51:36 +0100

    Init

---
+++ src/main/java/com/salesianostriana/campaing/CampaingApiApplication.java
@@ -0,0 +1,12 @@
+package com.salesianostriana.campaing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaingApiApplication.class, args);
+	}
+}
