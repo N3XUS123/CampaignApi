e79aca1f500ba18712864492664730c87f62ddf2
CCDDmcconejero <centenoconejeromario@gmail.com>
2018-11-06 22:46:32 +0100

    retocado login (no funciona)

--- src/main/resources/templates/CampaignApi/src/app/app.module.ts
+++ src/main/resources/templates/CampaignApi/src/app/app.module.ts
@@ -4,18 +4,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { MyMaterialModule } from './Login/login.material';
 import { RegistroComponent } from './registro/registro.component';
+import { LoginComponent } from './login/login.component';
+import { AuthService } from './services/auth.service';
+import { HttpClientModule } from '@angular/common/http';
  
 @NgModule({
   declarations: [
     AppComponent,
-    RegistroComponent
+    RegistroComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MyMaterialModule
+    MyMaterialModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
