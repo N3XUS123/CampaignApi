7108cf6199a7ecf4f0bd9bc0d801f9bd76d9e116
Your Namemcconejero <centenoconejeromario@gmail.com>
2018-11-06 13:31:14 +0100

    login programado con angular

--- src/main/resources/templates/CampaignApi/src/app/app.module.ts
+++ src/main/resources/templates/CampaignApi/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { MyMaterialModule } from './app.material';
+import { MyMaterialModule } from './Login/login.material';
  
 @NgModule({
   declarations: [
