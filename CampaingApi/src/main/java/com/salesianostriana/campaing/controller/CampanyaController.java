735bb8cb4d1312e9e0ff2c8875afce44abdf889b
Javier Romero Moreno <jromero@aulas.triana>
2018-11-08 09:22:16 +0100

    listar campañas

--- CampaingApi/src/main/java/com/salesianostriana/campaing/controller/CampanyaController.java
+++ CampaingApi/src/main/java/com/salesianostriana/campaing/controller/CampanyaController.java
@@ -1,5 +1,27 @@
 package com.salesianostriana.campaing.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.salesianostriana.campaing.model.Campanya;
+import com.salesianostriana.campaing.repository.CampanyaRepository;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
+
+@RestController
+@ApiOperation(value = "", authorizations = { @Authorization(value="token") })
 public class CampanyaController {
 
+	@Autowired
+	private CampanyaRepository repository;
+	
+	@GetMapping("/listarCampanyas")
+	@PreAuthorize("authenticated()")
+	public List<Campanya> listAll() {
+		return repository.findAll();
+	}
 }
--- CampaingApi/src/main/java/com/salesianostriana/campaing/service/CampanyaService.java
+++ CampaingApi/src/main/java/com/salesianostriana/campaing/service/CampanyaService.java
@@ -1,5 +1,5 @@
 package com.salesianostriana.campaing.service;
 
 public class CampanyaService {
-
+	
 }
