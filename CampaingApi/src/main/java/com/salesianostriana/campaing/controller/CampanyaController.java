da2ebf4f8dd50a0bdcee83cfa8b2c07a31e12ecb
Javier Romero Moreno <jromero@aulas.triana>
2018-11-08 09:57:19 +0100

    añadir campaña spring

--- CampaingApi/src/main/java/com/salesianostriana/campaing/controller/CampanyaController.java
+++ CampaingApi/src/main/java/com/salesianostriana/campaing/controller/CampanyaController.java
@@ -1,11 +1,16 @@
 package com.salesianostriana.campaing.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.salesianostriana.campaing.model.Campanya;
 import com.salesianostriana.campaing.repository.CampanyaRepository;
 
@@ -23,5 +28,14 @@ public class CampanyaController {
 	@PreAuthorize("hasRole('USER')")
 	public List<Campanya> listAll() {
 		return repository.findAll();
+	}
+	
+	@PostMapping("/campanya")
+	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<?> newCampanya(@RequestBody Campanya newCampanya) {
+		Campanya c = repository.save(newCampanya);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(c.getId()).toUri();
+
+		return ResponseEntity.created(location).body(c);
 	}
 }
