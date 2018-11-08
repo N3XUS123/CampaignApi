e54393f183fcc01f70faebe18eae9680a88f8a20
Your Namemcconejero <centenoconejeromario@gmail.com>
2018-11-08 10:53:26 +0100

    Borrar y editar datos maestros terminado

--- CampaingApi/src/main/java/com/salesianostriana/campaing/controller/DatosMaestrosController.java
+++ CampaingApi/src/main/java/com/salesianostriana/campaing/controller/DatosMaestrosController.java
@@ -5,12 +5,15 @@ import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import com.salesianostriana.campaing.model.DatosMaestros;
 import com.salesianostriana.campaing.repository.DatosMaestrosRepository;
 
@@ -21,7 +24,8 @@ public class DatosMaestrosController {
 	private DatosMaestrosRepository repo;
 	
 	//Listar
-	@GetMapping("/DatosMaestros")
+	@PreAuthorize("hasRole('USER')")
+	@GetMapping("/DatosMaestros/Lista")
 	public ResponseEntity<?> listarDatosMaestros(){
 		return ResponseEntity
 				.status(HttpStatus.ACCEPTED)
@@ -29,7 +33,8 @@ public class DatosMaestrosController {
 	}
 	
 	//Añadir
-	@PostMapping("/DatosMaestros")
+	@PreAuthorize("hasRole('ADMIN')")
+	@PostMapping("/DatosMaestros/Aniadir")
 	public ResponseEntity<?> newDatosMaestros(@RequestBody DatosMaestros newDatosMaestros) {
 		DatosMaestros datosMaestros = repo.save(newDatosMaestros);
 
@@ -38,5 +43,27 @@ public class DatosMaestrosController {
 
 		return ResponseEntity.created(location).body(datosMaestros);
 	}
+	
+	//Editar
+	@PreAuthorize("hasRole('ADMIN')")
+	@PutMapping("/DatosMaestros/Editar")
+	public DatosMaestros replaceDatosMaestros(@RequestBody DatosMaestros newDatosMaestros) {
+
+		return repo.findById(newDatosMaestros.getId()).map(datosMaestros -> {
+			datosMaestros.setTipo(newDatosMaestros.getTipo());
+			datosMaestros.setCampanya(newDatosMaestros.getCampanya());
+			return repo.save(datosMaestros);
+		}).orElseGet(() -> {
+			return repo.save(newDatosMaestros);
+		});
+	}
+	
+	//Delete
+	@PreAuthorize("hasRole('ADMIN')")
+	@DeleteMapping("/DatosMaestros/Borrar/{id}")
+	public void deleteDatosMaestros(@PathVariable Long id) {
+		repo.deleteById(id);
+	}
+
 	
 }
