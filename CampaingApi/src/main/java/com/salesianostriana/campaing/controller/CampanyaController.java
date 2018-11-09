35f10f1158f0040346dbd0d8d4f3d78eea03ac44
Javier Romero Moreno <jromero@aulas.triana>
2018-11-09 10:15:45 +0100

    eliminar campaña spring

--- CampaingApi/src/main/java/com/salesianostriana/campaing/controller/CampanyaController.java
+++ CampaingApi/src/main/java/com/salesianostriana/campaing/controller/CampanyaController.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,5 +39,13 @@ public class CampanyaController {
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(c.getId()).toUri();
 
 		return ResponseEntity.created(location).body(c);
+	}
+	
+	@DeleteMapping("/campanya/{id}")
+	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<?> deleteCampanya(@PathVariable Long id) {
+		repository.deleteById(id);
+
+		return ResponseEntity.noContent().build();
 	}
 }
