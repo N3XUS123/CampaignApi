a9e02687bb4be950be1f3e24a81a7dcc6053a2bc
Your Name <you@example.com>
2018-11-11 13:15:04 +0100

    aportaciones helpers y service

--- CampaingApi/src/main/java/com/salesianostriana/campaing/formbean/AportacionDto.java
+++ CampaingApi/src/main/java/com/salesianostriana/campaing/formbean/AportacionDto.java
@@ -8,8 +8,24 @@ import lombok.Data;
 @Data @AllArgsConstructor
 public class AportacionDto {
 
+	private long id;
 	private String dato;
 	private double cantidad;
 	private LocalDateTime fecha;
+	private long idCampanya;
+	private long idUsuario;
+	private long idDatosMaestro;
+	
+	public AportacionDto(String dato, double cantidad, LocalDateTime fecha, long idCampanya, long idUsuario,
+			long idDatosMaestro) {
+		
+		this.dato = dato;
+		this.cantidad = cantidad;
+		this.fecha = fecha;
+		this.idCampanya = idCampanya;
+		this.idUsuario = idUsuario;
+		this.idDatosMaestro = idDatosMaestro;
+	}
+	
 	
 }
--- CampaingApi/src/main/java/com/salesianostriana/campaing/service/AportacionService.java
+++ CampaingApi/src/main/java/com/salesianostriana/campaing/service/AportacionService.java
@@ -7,6 +7,9 @@ import com.salesianostriana.campaing.formbean.AportacionDto;
 import com.salesianostriana.campaing.model.Aportacion;
 import com.salesianostriana.campaing.model.Usuario;
 import com.salesianostriana.campaing.repository.AportacionRepository;
+import com.salesianostriana.campaing.repository.CampanyaRepository;
+import com.salesianostriana.campaing.repository.DatosMaestrosRepository;
+import com.salesianostriana.campaing.repository.UsuarioRepository;
 
 @Service
 public class AportacionService {
@@ -14,10 +17,20 @@ public class AportacionService {
 	@Autowired
 	private AportacionRepository repo;
 	
-
+	@Autowired
+	private CampanyaRepository cRepo;
+	
+	@Autowired
+	private DatosMaestrosRepository dRepo;
+	
+	@Autowired
+	private UsuarioRepository uRepo;
 	
 	public Aportacion save(AportacionDto nuevaAportacion, Usuario u) {
 		Aportacion a = new Aportacion(nuevaAportacion.getDato(), nuevaAportacion.getCantidad(), nuevaAportacion.getFecha(), u);
+		a.addCampanya(cRepo.findById(nuevaAportacion.getIdCampanya()).orElse(null));
+		a.addDatosMaestros(dRepo.findById(nuevaAportacion.getIdDatosMaestro()).orElse(null));
+		a.addUsuario(uRepo.findById(nuevaAportacion.getIdUsuario()).orElse(null));
 		return repo.save(a);
 	}
 }
