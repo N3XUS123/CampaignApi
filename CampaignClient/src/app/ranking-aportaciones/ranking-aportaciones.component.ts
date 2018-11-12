d161de564cf9e206f6a16a5e2a6cd4a51b05709a
N3XUS123 <lPalma.esp123@gmail.com>
2018-11-12 11:33:40 +0100

    Creado ranking

---
+++ CampaignClient/src/app/ranking-aportaciones/ranking-aportaciones.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ranking-aportaciones',
+  templateUrl: './ranking-aportaciones.component.html',
+  styleUrls: ['./ranking-aportaciones.component.css']
+})
+export class RankingAportacionesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+  
+
+}
