21564484ae3faed427e4d324927fb79f8931043a
N3XUS123 <lPalma.esp123@gmail.com>
2018-11-14 12:41:20 +0100

    Ranking en API

--- CampaingApi/src/main/java/com/salesianostriana/campaing/repository/AportacionRepository.java
+++ CampaingApi/src/main/java/com/salesianostriana/campaing/repository/AportacionRepository.java
@@ -8,12 +8,15 @@ import org.springframework.data.jpa.repository.Query;
 import com.salesianostriana.campaing.model.Aportacion;
 import com.salesianostriana.campaing.model.Campanya;
 import com.salesianostriana.campaing.model.Usuario;
+import com.salesianostriana.campaing.response.RankingQueryResponse;
 
 public interface AportacionRepository extends JpaRepository<Aportacion, Long> {
 
-	@Query(value = "SELECT * FROM APORTACION WHERE CAMPANYA = ?1 GROUP BY USUARIO ORDER BY CANTIDAD LIMIT 5", nativeQuery = true)
-	public List<Aportacion> rankingTotal(long idCampanya);
+	@Query(value = "SELECT USUARIO AS U, SUM(CANTIDAD) AS C FROM APORTACION WHERE CAMPANYA = ?1 GROUP BY DATO_MAESTRO ORDER BY SUM(CANTIDAD)", nativeQuery = true)
+	public List<RankingQueryResponse> rankingTotal(long idCampanya);
+	
 
+	
 	@Query(value= "SELECT * FROM APORTACION WHERE USUARIO = ?1 AND CAMPANYA = ?2", nativeQuery = true)
 	public List<Aportacion> allMyCampaignsContributions(Usuario u, Campanya c);
 	
---
+++ CampaingApi/src/main/java/com/salesianostriana/campaing/response/RankingResponse.java
@@ -0,0 +1,12 @@
+package com.salesianostriana.campaing.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data @AllArgsConstructor
+public class RankingResponse {
+	
+	private String grupo;
+	private int Cantidad;
+	
+}
