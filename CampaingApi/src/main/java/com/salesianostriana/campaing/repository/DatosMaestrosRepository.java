f307ddc14e7e516fc29b8bf3bc2898c0d588b91b
N3XUS123 <lPalma.esp123@gmail.com>
2018-11-14 09:38:48 +0100

    Fix DataMaster from Campaign

--- CampaingApi/src/main/java/com/salesianostriana/campaing/repository/DatosMaestrosRepository.java
+++ CampaingApi/src/main/java/com/salesianostriana/campaing/repository/DatosMaestrosRepository.java
@@ -9,6 +9,6 @@ import com.salesianostriana.campaing.model.DatosMaestros;
 
 public interface DatosMaestrosRepository extends JpaRepository<DatosMaestros, Long>{
 
-	@Query(value = "SELECT * FROM DATOSMAESTROS WHERE CAMPANYA = ?1")
+	@Query(value = "SELECT * FROM DATOS_MAESTROS WHERE CAMPANYA_ID = ?1", nativeQuery = true)
 	public List<DatosMaestros> listarDatosCampanya(long idCampanya);
 }
